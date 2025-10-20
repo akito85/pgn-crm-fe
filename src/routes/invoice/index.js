8e5794776016718176c55d18a278452c01658a20
tegarfaris <tegarfarisn@gmail.com>
2025-10-20 14:29:15 +0700

    chore: added comment message at invoice routes

--- src/routes/invoice/index.js
+++ src/routes/invoice/index.js
@@ -1,9 +1,10 @@
 import { INVOICE_ELEMENTS } from "./invoice_elements";
 import { INVOICE_ROUTES } from "./invoice_routes";
 
+// invoice routes
 export const invoice = [
-    {
-        path: INVOICE_ROUTES.GENERATE_INVOICE_VIEW,
-        element: INVOICE_ELEMENTS.GENERATE_INVOICE_VIEW_PAGE
-    }
-]
+  {
+    path: INVOICE_ROUTES.GENERATE_INVOICE_VIEW,
+    element: INVOICE_ELEMENTS.GENERATE_INVOICE_VIEW_PAGE,
+  },
+];
