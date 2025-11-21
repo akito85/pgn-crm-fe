c0b35b057d5d2137a650af090c09462df1bc8552
Jonathan Paskalis <ymjonathangp2@gmail.com>
2025-11-21 14:57:01 +0700

    feat(payment relation): duplicate a dropdown - duplicate the tax implication dropdown

--- src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/FinancialInformation.js
+++ src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/FinancialInformation.js
@@ -79,6 +79,10 @@ const FinancialInformation = ({ id = 0 }) => {
       header: "Tax Implication",
       children: <TaxImplication id={id} />,
     },
+    {
+      header: "Payment Relation",
+      children: <TaxImplication id={id}/>,
+    },
   ];
 
   // useEffect(() => {
