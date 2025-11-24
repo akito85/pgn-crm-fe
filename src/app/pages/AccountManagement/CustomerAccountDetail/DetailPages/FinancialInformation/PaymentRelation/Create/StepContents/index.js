e331e62865c8b6f6a9ac646acd44353621f33ba4
Jonathan Paskalis <ymjonathangp2@gmail.com>
2025-11-24 11:30:36 +0700

    feat(payment relation): updated the page - changed to the correct page name - added routes

--- src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/PaymentRelation/Create/StepContents/index.js
+++ src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/PaymentRelation/Create/StepContents/index.js
@@ -1,13 +1,9 @@
 import InformationForm from "./InformationForm";
 import AttachmentForm from "./AttachmentForm";
 import ApprovalForm from "./ApprovalForm";
-import ContactForm from "./ContactForm";
-import PreRequisiteForm from "./PreRequisiteForm";
 
 const StepContents = {
   InformationForm,
-  ContactForm,
-  PreRequisiteForm,
   AttachmentForm,
   ApprovalForm
 };
