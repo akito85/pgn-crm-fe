0a473a5673ba8f8f17b16a7fbb44d86ff55b99fa
Jonathan Paskalis <ymjonathangp2@gmail.com>
2025-11-24 11:16:15 +0700

    feat(payment relation): copied create files from customer service request

---
+++ src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/PaymentRelation/Create/StepContents/index.js
@@ -0,0 +1,15 @@
+import InformationForm from "./InformationForm";
+import AttachmentForm from "./AttachmentForm";
+import ApprovalForm from "./ApprovalForm";
+import ContactForm from "./ContactForm";
+import PreRequisiteForm from "./PreRequisiteForm";
+
+const StepContents = {
+  InformationForm,
+  ContactForm,
+  PreRequisiteForm,
+  AttachmentForm,
+  ApprovalForm
+};
+
+export default StepContents;
