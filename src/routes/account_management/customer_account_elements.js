db4962daf436ae76ab35d302b0dc05b5ae3942d5
Jonathan Paskalis <ymjonathangp2@gmail.com>
2025-11-24 17:13:17 +0700

    feat(payment relation): changed create file name to createUpdate

--- src/routes/account_management/customer_account_elements.js
+++ src/routes/account_management/customer_account_elements.js
@@ -48,7 +48,7 @@ import EquipmentForm from "../../app/pages/AccountManagement/CustomerAccountDeta
 import RawMaterialSourceForm from "../../app/pages/AccountManagement/CustomerAccountDetail/DetailPages/RawMaterialSource/RawMaterialSourceHistory/RawMaterialSourceForm";
 import ProductDistributionForm from "../../app/pages/AccountManagement/CustomerAccountDetail/DetailPages/ProductDistribution/ProductDistributionHistory/ProductDistributionForm";
 import PaymentRelationDetails from "../../app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/PaymentRelation/Details/PaymentRelationDetails"; 
-import CreatePaymentRelation from "../../app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/PaymentRelation/Create/CreatePaymentRelation"
+import CreatePaymentRelation from "../../app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/PaymentRelation/CreateUpdate/CreateUpdatePaymentRelation"
 
 export const ACCOUNT_MANAGEMENT_ELEMENTS = {
   // Customer/Account List
