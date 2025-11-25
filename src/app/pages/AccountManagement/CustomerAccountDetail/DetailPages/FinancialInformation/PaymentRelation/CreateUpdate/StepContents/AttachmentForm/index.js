120bff86993d5658fceea70d3678498393571ee2
Jonathan Paskalis <ymjonathangp2@gmail.com>
2025-11-25 16:15:53 +0700

    feat(payment relation): copied attachment step from product form

--- src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/PaymentRelation/CreateUpdate/StepContents/AttachmentForm/index.js
+++ src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/PaymentRelation/CreateUpdate/StepContents/AttachmentForm/index.js
@@ -1,3 +1,23 @@
-export default function AttachmentForm(){
-  return(<div>Attachment Form</div>)
+import AttachmentPaymentRelation from "./AttachmentPaymentRelation";
+
+export default function AttachmentForm({
+  type,
+  data,
+  updateData,
+  typeSelector,
+  dispatch,
+  getAPICategory,
+  mandatory,
+}){
+  return(
+    <AttachmentPaymentRelation
+      type={type}
+      data={data}
+      updateData={updateData}
+      typeSelector={typeSelector}
+      dispatch={dispatch}
+      getAPICategory={getAPICategory}
+      mandatory={mandatory}
+    />
+  )
 }
