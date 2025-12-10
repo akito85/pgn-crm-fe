6b2e7e046586a948b29fb1dc872f93786fc1d76c
Jonathan Paskalis <ymjonathangp2@gmail.com>
2025-12-10 16:19:58 +0700

    fix(payment relation): fixed create model info tab

--- src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/PaymentRelation/CreateUpdate/ConfirmationModal/ConfirmationModalInfo.js
+++ src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/PaymentRelation/CreateUpdate/ConfirmationModal/ConfirmationModalInfo.js
@@ -1,61 +1,11 @@
-import { useState, useEffect, useRef } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { useNavigate, useLocation } from 'react-router-dom';
 import { Fragment } from "react";
 import DetailText from "../../../../../../../../../components/DetailText";
-import { getCustomerDetail } from "../../../../../../../../../redux/slices/account_management/Customer/customerAccount";
 import moment from "moment";
 import { dateFormatting } from "../../../../../../../../../utils";
 
 const ConfirmationModalInfo = ({
   data = {},
-  handleChange = () => {},
-  handleChangeSize = () => {},
-  totalElement = 0,
-  page = 1,
-  pageSize = 10,
-  searchText = "",
-  searchedColumn = "",
-  onSort = () => {},
-  getColumnSearchProps = () => {},
-  searchInput,
-  handleSearch
 }) => {
-  // State
-  const [dataDetail, setDataDetail] = useState({});
-  const dispatch = useDispatch();
-  const [SRStatus, setSRStatus] = useState(null);
-
-  const { data_customerDetail, loading, loadingAccount } = useSelector(
-    (state) => state.customerAccount
-    );
-  const { access_account } = useSelector(
-    (state) => state.accountManagement
-  );
-  
-  const isLoading = loading || loadingAccount;
-
-  //declare
-  const navigate = useNavigate();
-  const location = useLocation();
-  const id = location?.state?.id;
-
-  useEffect(() => {
-    if (id) {
-      dispatch(getCustomerDetail(id));
-    }
-  }, [dispatch, id]);
-
-  const InfoDummy = {
-    accountNumber: "2027635461",
-    accountName: "PT XYZ",
-    priority: "1",
-    startDate: "22 Aug 2022",
-    endDate: "22 Aug 2022",
-    status: "Active",
-    description: "Lorem ipsum dolor sit amet consectetur. Malesuada turpis arcu morbi elit sed lorem at adipiscing imperdiet. Aliquam quis tempus feugiat amet. Viverra metus tincidunt nibh mauris nisi. At et etiam non dignissim ultricies tellus in lacus fermentum. Sollicitudin purus viverra tincidunt proin."
-  };
-
   const renderDate = (date) => {
     if (date) {
       return moment(date).format(dateFormatting.dateTime);
@@ -70,14 +20,14 @@ const ConfirmationModalInfo = ({
         PAYMENT RELATION INFORMATION
       </div>
       <div className="w-full grid grid-cols-3 gap-x-5">
-        <DetailText label="Account Number">{InfoDummy.customerNumber}</DetailText>
-        <DetailText label="Account Name">{InfoDummy.identificationType}</DetailText>
-        <DetailText label="Priority">{InfoDummy.customerIdentificationNumber}</DetailText>
-        <DetailText label="Start Date">{renderDate(InfoDummy.startDate)}</DetailText>
-        <DetailText label="End Date">{renderDate(InfoDummy.endDate)}</DetailText>
+        <DetailText label="Account Number">{data.accountNumber}</DetailText>
+        <DetailText label="Account Name">{data.accountName}</DetailText>
+        <DetailText label="Priority">{data.priority}</DetailText>
+        <DetailText label="Start Date">{renderDate(data.startDate)}</DetailText>
+        <DetailText label="End Date">{renderDate(data.endDate)}</DetailText>
       </div>
       <div className="w-full">
-        <DetailText label="Description">{InfoDummy.description}</DetailText>
+        <DetailText label="Description">{data.description}</DetailText>
       </div>
     </Fragment>
   );
--- src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/PaymentRelation/CreateUpdate/ConfirmationModal/ConfirmationModalTabs.js
+++ src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/PaymentRelation/CreateUpdate/ConfirmationModal/ConfirmationModalTabs.js
@@ -19,6 +19,7 @@ const ConfirmationModalTabs = ({
   hierarchyTableData,
   dispatch,
   dataAttachment,
+  data = {}
 }) => {
   // Use provided options or fall back to default tabs
   const tabOptions = options.length > 0 ? options : [
@@ -30,7 +31,7 @@ const ConfirmationModalTabs = ({
   const renderSection = () => {
     switch (section) {
       case dataTabs.info:
-        return <ConfirmationModalInfo />;
+        return <ConfirmationModalInfo data={data} />;
       case dataTabs.apprv:
         return (
         <ConfirmationModalApproval
