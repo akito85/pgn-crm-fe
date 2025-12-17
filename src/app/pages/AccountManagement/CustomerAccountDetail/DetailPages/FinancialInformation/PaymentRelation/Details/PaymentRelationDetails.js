3d705b51a579ca0998d77b717d0b84321761555f
Jonathan Paskalis <ymjonathangp2@gmail.com>
2025-12-17 09:43:32 +0700

    fix(payment relation): fixed detail approval - changed condition on which PR is using reguler or inactive approval

--- src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/PaymentRelation/Details/PaymentRelationDetails.js
+++ src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/PaymentRelation/Details/PaymentRelationDetails.js
@@ -127,7 +127,7 @@ const PaymentRelationDetails = ({
         description,
       }];
 
-      if (!result.approvalType) {
+      if (result.approvalType === "PAYMENT_RELATION") {
         dispatch(approveOrRejectPaymentRelation({
           body,
           action,
@@ -139,7 +139,7 @@ const PaymentRelationDetails = ({
           handleApprovalModal(false);
         })
         .catch(() => {});
-      } else if (result.approvalType) {
+      } else if (result.approvalType === "INACTIVE_PAYMENT_RELATION") {
         dispatch(approveOrRejectInactivePaymentRelation({
           body,
           action,
