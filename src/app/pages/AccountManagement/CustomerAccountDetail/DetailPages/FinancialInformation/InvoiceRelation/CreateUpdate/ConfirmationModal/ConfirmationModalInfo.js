287fe31ddaabfaf5bc4aaef6a10a4921510091db
Jonathan Paskalis <ymjonathangp2@gmail.com>
2025-12-17 20:18:50 +0700

    fix(invoice relation): fixed create/update form - fixed confirmation modal info tab

--- src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/InvoiceRelation/CreateUpdate/ConfirmationModal/ConfirmationModalInfo.js
+++ src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/InvoiceRelation/CreateUpdate/ConfirmationModal/ConfirmationModalInfo.js
@@ -12,9 +12,9 @@ const ConfirmationModalInfo = ({
         INVOICE RELATION INFORMATION
       </div>
       <div className="w-full grid grid-cols-3 gap-x-5">
-        <DetailText label="Account Number">{data.customerNumber}</DetailText>
-        <DetailText label="Account Name">{data.identificationType}</DetailText>
-        <DetailText label="Priority">{data.customerIdentificationNumber}</DetailText>
+        <DetailText label="Account Number">{data.accountNumber}</DetailText>
+        <DetailText label="Account Name">{data.accountName}</DetailText>
+        <DetailText label="Priority">{data.priority}</DetailText>
         <DetailText label="Start Date">{moment(data.startDate, dateFormatting.f_date).format(dateFormatting.date)}</DetailText>
         <DetailText label="End Date">{data.endDate ? moment(data.endDate, dateFormatting.f_date).format(dateFormatting.date) : ""}</DetailText>
       </div>
