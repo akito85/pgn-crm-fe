d12b12dc9f4d0c6b9adbf884cef5ec280d704c9d
Jonathan Paskalis <ymjonathangp2@gmail.com>
2025-12-17 11:40:19 +0700

    feat(invoice relation): updated get account list endpoint

--- src/redux/slices/account_management/detailAccount/FinancialInformationSlice.js
+++ src/redux/slices/account_management/detailAccount/FinancialInformationSlice.js
@@ -834,7 +834,7 @@ export const getIrAccountStandard = createAsyncThunk(
 
       const sortParams =
         sort === undefined || sort === "" ? "createdDate~desc" : sort;
-      const url = `/v1/dbs/api/account/list?searchs=${searchParams}&page=${page}&size=${pageSize}&sort=${sortParams}`;
+      const url = `/v1/dbs/api/invoice-relation/list-account?searchs=${searchParams}&page=${page}&size=${pageSize}&sort=${sortParams}`;
       const response = await accountManagementService.getPagination(url);
       return response?.data;
     } catch (error) {
