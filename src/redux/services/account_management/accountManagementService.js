0559074f9b409b70307451b0916c66a8b4943b91
Jonathan Paskalis <ymjonathangp2@gmail.com>
2025-12-18 15:50:30 +0700

    debug(invoice relation): debug download

--- src/redux/services/account_management/accountManagementService.js
+++ src/redux/services/account_management/accountManagementService.js
@@ -169,6 +169,7 @@ const uploadAttachment = async (url, body) => {
 //download advanced
 const downloadDataAdvanced = async (url, body) => {
   try {
+    console.log("masuk try");
     const response = await axios.post(configApp.ACCOUNT_SERVICE + url, body, {
       headers: {
         ...tokenHeader(),
@@ -176,7 +177,9 @@ const downloadDataAdvanced = async (url, body) => {
       },
       responseType: "blob",
     });
+    console.log("response try", response);
     if(hasValue(response.headers?.get("content-disposition"))){
+      console.log('masuk hasValue(response.headers?.get("content-disposition"))');
       const filename = response.headers
         .get("content-disposition")
         .split(";")
@@ -185,13 +188,17 @@ const downloadDataAdvanced = async (url, body) => {
         .trim();
   
       const blob = await response.data;
+      console.log("blob", blob);
   
       // Download the file
       FileSaver.saveAs(blob, filename);
     } else {
+      console.log("masuk else");
       return response
     }
   } catch (error) {
+    console.log("masuk try");
+    console.log("error catch", error);
     throw error;
   }
 };
