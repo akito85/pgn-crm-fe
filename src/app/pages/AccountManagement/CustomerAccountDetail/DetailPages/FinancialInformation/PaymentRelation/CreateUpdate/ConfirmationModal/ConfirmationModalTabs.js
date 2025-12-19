17dd09be64b5003952ca8ed5d86dd1fc69cc31f1
Jonathan Paskalis <ymjonathangp2@gmail.com>
2025-12-19 14:47:53 +0700

    fix(payment relation): fixed create/update form - fixed update confirmation modal download attachment

--- src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/PaymentRelation/CreateUpdate/ConfirmationModal/ConfirmationModalTabs.js
+++ src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/PaymentRelation/CreateUpdate/ConfirmationModal/ConfirmationModalTabs.js
@@ -19,7 +19,8 @@ const ConfirmationModalTabs = ({
   hierarchyTableData,
   dispatch,
   dataAttachment,
-  data = {}
+  data = {},
+  service,
 }) => {
   // Use provided options or fall back to default tabs
   const tabOptions = options.length > 0 ? options : [
@@ -44,6 +45,7 @@ const ConfirmationModalTabs = ({
         return <ConfirmationModalAttachment
           data={dataAttachment}
           dispatch={dispatch}
+          service={service}
         />;
       default:
         return "Payment Relation Information";
