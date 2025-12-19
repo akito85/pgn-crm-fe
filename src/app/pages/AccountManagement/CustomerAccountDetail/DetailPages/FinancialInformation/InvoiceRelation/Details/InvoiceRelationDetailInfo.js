3535d3c204b5dffd31481a790dbd1bd8d3349e79
Jonathan Paskalis <ymjonathangp2@gmail.com>
2025-12-19 13:55:00 +0700

    fix(invoice relation): developed detail page - developed subject and object logic to display "accountNumber" and "accountName"

--- src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/InvoiceRelation/Details/InvoiceRelationDetailInfo.js
+++ src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/InvoiceRelation/Details/InvoiceRelationDetailInfo.js
@@ -5,6 +5,7 @@ import moment from "moment";
 import { dateFormatting } from "../../../../../../../../utils";
 
 const InvoiceRelationDetailInfo = ({
+  subjectAccountNumber,
   dataDetail = {},
 }) => {
   return (
@@ -12,26 +13,26 @@ const InvoiceRelationDetailInfo = ({
       <BaseContainer header={"INVOICE RELATION"}>
         <div className="w-full grid grid-cols-3 gap-4">
           {/* Invoice Relation Information */}
-          <DetailText label="Account Number">{dataDetail?.relatedAccountNumber}</DetailText>
-          <DetailText label="Account Name">{dataDetail?.relatedAccountName}</DetailText>
-          <DetailText label="Priority">{dataDetail?.priority}</DetailText>
-          <DetailText label="Start Date">{dataDetail?.startDate ? moment(dataDetail.startDate, dateFormatting.f_date).format(dateFormatting.date) : ""}</DetailText>
-          <DetailText label="End Date">{dataDetail?.endDate ? moment(dataDetail.endDate, dateFormatting.f_date).format(dateFormatting.date) : ""}</DetailText>
-          <DetailText label="Status">{dataDetail?.status}</DetailText>
+          <DetailText label="Account Number">{subjectAccountNumber === dataDetail.accountNumber ? dataDetail.relatedAccountNumber : dataDetail.accountNumber}</DetailText>
+          <DetailText label="Account Name">{subjectAccountNumber === dataDetail.accountNumber ? dataDetail.relatedAccountName : dataDetail.accountNumber}</DetailText>
+          <DetailText label="Priority">{dataDetail.priority}</DetailText>
+          <DetailText label="Start Date">{dataDetail.startDate ? moment(dataDetail.startDate, dateFormatting.f_date).format(dateFormatting.date) : ""}</DetailText>
+          <DetailText label="End Date">{dataDetail.endDate ? moment(dataDetail.endDate, dateFormatting.f_date).format(dateFormatting.date) : ""}</DetailText>
+          <DetailText label="Status">{dataDetail.status}</DetailText>
         </div>
         <div className="w-full">
-          <DetailText label="Description">{dataDetail?.description}</DetailText>
+          <DetailText label="Description">{dataDetail.description}</DetailText>
         </div>
       </BaseContainer>
 
       <BaseContainer header={"HISTORY LOG INFORMATION"}>
         <div className="w-full grid grid-cols-5 gap-4">
           {/* History Log Information */}
-          <DetailText label="Record Id">{dataDetail?.id}</DetailText>
-          <DetailText label="Created Date">{dataDetail?.createdDate ? moment(dataDetail.createdDate, dateFormatting.meas_date).format(dateFormatting.dateTime) : ""}</DetailText>
-          <DetailText label="Created By">{dataDetail?.createdBy}</DetailText>
-          <DetailText label="Updated Date">{dataDetail?.updatedDate ? moment(dataDetail.updatedDate, dateFormatting.meas_date).format(dateFormatting.dateTime) : ""}</DetailText>
-          <DetailText label="Updated By">{dataDetail?.updatedBy}</DetailText>
+          <DetailText label="Record Id">{dataDetail.id}</DetailText>
+          <DetailText label="Created Date">{dataDetail.createdDate ? moment(dataDetail.createdDate, dateFormatting.meas_date).format(dateFormatting.dateTime) : ""}</DetailText>
+          <DetailText label="Created By">{dataDetail.createdBy}</DetailText>
+          <DetailText label="Updated Date">{dataDetail.updatedDate ? moment(dataDetail.updatedDate, dateFormatting.meas_date).format(dateFormatting.dateTime) : ""}</DetailText>
+          <DetailText label="Updated By">{dataDetail.updatedBy}</DetailText>
         </div>
       </BaseContainer>
     </Fragment>
