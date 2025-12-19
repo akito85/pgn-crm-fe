36b045c777c9f2f148da63ac66486d7588831c9d
Jonathan Paskalis <ymjonathangp2@gmail.com>
2025-12-19 14:00:05 +0700

    fix(invoice relation): fixed detail page - fixed "accountName" info

--- src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/InvoiceRelation/Details/InvoiceRelationDetailInfo.js
+++ src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/InvoiceRelation/Details/InvoiceRelationDetailInfo.js
@@ -14,7 +14,7 @@ const InvoiceRelationDetailInfo = ({
         <div className="w-full grid grid-cols-3 gap-4">
           {/* Invoice Relation Information */}
           <DetailText label="Account Number">{subjectAccountNumber === dataDetail.accountNumber ? dataDetail.relatedAccountNumber : dataDetail.accountNumber}</DetailText>
-          <DetailText label="Account Name">{subjectAccountNumber === dataDetail.accountNumber ? dataDetail.relatedAccountName : dataDetail.accountNumber}</DetailText>
+          <DetailText label="Account Name">{subjectAccountNumber === dataDetail.accountNumber ? dataDetail.relatedAccountName : dataDetail.accountName}</DetailText>
           <DetailText label="Priority">{dataDetail.priority}</DetailText>
           <DetailText label="Start Date">{dataDetail.startDate ? moment(dataDetail.startDate, dateFormatting.f_date).format(dateFormatting.date) : ""}</DetailText>
           <DetailText label="End Date">{dataDetail.endDate ? moment(dataDetail.endDate, dateFormatting.f_date).format(dateFormatting.date) : ""}</DetailText>
