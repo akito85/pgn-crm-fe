22b341ed7265962b67663fd2217db9d17a50d3b8
Jonathan Paskalis <ymjonathangp2@gmail.com>
2025-12-26 13:37:38 +0700

    fix(payment relation): fixed update auth - forbid update for "active" record

--- src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/PaymentRelation/PaymentRelationTable.js
+++ src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/PaymentRelation/PaymentRelationTable.js
@@ -243,7 +243,7 @@ const PaymentRelationTable = ({
               idAccount,
               idCustomer,
             }})}
-            disabled={r.statusApproval === "WAITING_APPROVAL" || r.status === "INACTIVE"}
+            disabled={r.statusApproval === "WAITING_APPROVAL" || r.status === "INACTIVE" || r.status === "ACTIVE"}
           >
             <Tooltip title="Update">
               <div className="pt-1">
