2dcec9de36c23f7c0d83a35cec53aecb68921407
Vitrio Wibisono <vitrio@noxus.co.id>
2025-12-26 17:11:28 +0700

    Removed IconPreview route

--- src/routes/notification/index.js
+++ src/routes/notification/index.js
@@ -3,12 +3,10 @@ import IconPreview from "../../app/pages/Notifications/IconPreview"
 
 export const NOTIFICATION_ELEMENT = {
   VIEW_NOTIFICATION_PAGE: <NotificationHistory />,
-  ICON_PREVIEW_PAGE: <IconPreview />
 }
 
 export const NOTIFICATION_ROUTE = {
   VIEW_NOTIFICATION: "/notifications/view",
-  ICON_PREVIEW: "/notifications/iconPreview",
 }
 
 export const notification = [
@@ -16,8 +14,4 @@ export const notification = [
     path: NOTIFICATION_ROUTE.VIEW_NOTIFICATION,
     element: NOTIFICATION_ELEMENT.VIEW_NOTIFICATION_PAGE
   },
-  {
-    path: NOTIFICATION_ROUTE.ICON_PREVIEW,
-    element: NOTIFICATION_ELEMENT.ICON_PREVIEW_PAGE
-  }
 ]
