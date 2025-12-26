f7345f0bd9dff95b278d6c0eae38224436a128a7
Vitrio Wibisono <vitrio@noxus.co.id>
2025-12-26 17:12:59 +0700

    Fix merge conflict leftover

--- src/redux/stores/store.js
+++ src/redux/stores/store.js
@@ -119,12 +119,8 @@ import contentManagementReducer from "../slices/rating_billing_invoice/MasterDat
 import accountPromoReducer from "../../app/pages/AccountManagement/CustomerAccountDetail/DetailPages/Promo/store/slices/promoSlice";
 import accountingReducer from "../slices/receipt_collection/accounting";
 import relationshipReducer from "../slices/account_management/detailAccount/relationshipSlice";
-<<<<<<< HEAD
 import notificationsReducer from "../slices/notifications";
-||||||| 987494ad
-=======
 import multiDestinationReducer from "../slices/account_management/detailAccount/MultiDestinationSlice"
->>>>>>> 3ec5dda041f4c01fe0cabd21a8887da59337753e
 
 const reducer = combineReducers({
   job: jobSlice,
