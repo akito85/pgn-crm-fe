fbfb5a498bbae536009b22396ab64da66fa2d457
fariz_triklin-rekatama <fariznurzman@triklin-rekatama.co.id>
2025-12-29 22:24:39 +0700

    fix: Stores.js Warranty

--- src/redux/stores/store.js
+++ src/redux/stores/store.js
@@ -119,6 +119,7 @@ import contentManagementReducer from "../slices/rating_billing_invoice/MasterDat
 import accountPromoReducer from "../../app/pages/AccountManagement/CustomerAccountDetail/DetailPages/Promo/store/slices/promoSlice";
 import accountingReducer from "../slices/receipt_collection/accounting";
 import relationshipReducer from "../slices/account_management/detailAccount/relationshipSlice";
+import warrantyReducer from "../slices/receipt_collection/warranty";
 
 const reducer = combineReducers({
   job: jobSlice,
@@ -182,6 +183,7 @@ const reducer = combineReducers({
   accountEquipment: equpmentReducer,
   accountPromo: accountPromoReducer,
   relationship: relationshipReducer,
+  warranty: warrantyReducer,
 
   //Account Management ( Customer )
   customerAccount: customerAccountReducer,
