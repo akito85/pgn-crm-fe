8ffb03d79c3a329bf8e1559e0762f76296ecf260
Jonathan Paskalis <ymjonathangp2@gmail.com>
2025-12-31 09:31:38 +0700

    fix(invoice relation): fixed empty table display

--- src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/InvoiceRelation/InvoiceRelationTable.js
+++ src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/InvoiceRelation/InvoiceRelationTable.js
@@ -10,6 +10,7 @@ import { useColumnActionPermission } from "../../../../../../../components/Colum
 import ButtonComponent from "../../../../../../../components/ButtonComponent";
 import { CheckOutlined, DownloadOutlined, FilterOutlined, PlusOutlined } from "@ant-design/icons";
 import Toolbar from "../../../../../../../components/Toolbar";
+import TablePaginationNew from "../../../../../../../components/TablePaginationNew";
 
 const InvoiceRelationTable = ({
   data = [],
@@ -334,14 +335,13 @@ const InvoiceRelationTable = ({
           <Toolbar items={itemActions} type="detail" />
         </div>
       )}
-      <TablePagination
+      <TablePaginationNew
         dataSource={data}
         totalData={totalElement}
         current={page}
         pageSize={pageSize}
-        onChange={handleChange}
-        onSizeChanger={handleChangeSize}
-        tableScrolled={{ y: 400, x: "max-content" }}
+        onChange={handleChangeSize}
+        tableScrolled={{ y: 400, x: data.length ? "max-content" : "100%" }}
         onSort={onSort}
         columns={[
           ...columns,
