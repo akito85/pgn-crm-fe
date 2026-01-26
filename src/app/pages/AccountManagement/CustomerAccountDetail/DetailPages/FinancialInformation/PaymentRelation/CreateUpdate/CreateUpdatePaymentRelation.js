503fbf6b48722fc521ac2cc2b4929160dffdedf9
Jonathan Paskalis <ymjonathangp2@gmail.com>
2026-01-26 16:46:32 +0700

    feat(payment relation): devd create/update page - changed account and customer info cards UI

--- src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/PaymentRelation/CreateUpdate/CreateUpdatePaymentRelation.js
+++ src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/PaymentRelation/CreateUpdate/CreateUpdatePaymentRelation.js
@@ -39,6 +39,7 @@ import {
 import { showModalError, validateCreateUpdate } from "../../../../../../../../redux/slices/general_slice";
 import accountManagementService from "../../../../../../../../redux/services/account_management/accountManagementService";
 import { configApp } from "../../../../../../../../constants/configApp";
+import CardContainer from "../../../../../../../../components/CardContainer";
 
 const CreatePaymentRelation = ({ type }) => {
   const containerRef = useRef(null);
@@ -630,53 +631,44 @@ const CreatePaymentRelation = ({ type }) => {
     <LayoutMenu>
       <div className="flex flex-col gap-y-5">
         <BreadCrumb routes={routes} />
-        <BaseContainer removeTopMargin>
-          <div className="flex flex-col gap-y-5">
-            {/* Customer Information */}
-            <div className="text-primary text-xs font-bold uppercase">
-              CUSTOMER INFORMATION
-            </div>
-            <div className="w-full grid grid-cols-4 gap-x-5">
-              <DetailText label="Customer Number">{data_customerDetail?.customerNumber}</DetailText>
-              <DetailText label="Identification Type">{data_customerDetail?.identificationType}</DetailText>
-              <DetailText label="Customer Identification Number">{data_customerDetail?.customerIdentificationNumber}</DetailText>
-              <DetailText label="Customer Name">{data_customerDetail?.customerName}</DetailText>
-              <DetailText label="Customer Type">{data_customerDetail?.customerType}</DetailText>
-              <DetailText label="Description">{data_customerDetail?.description}</DetailText>
-              <DetailText label="Birth/Founded Date">{renderDate(data_customerDetail?.birthFoundedDate)}</DetailText>
-              <DetailText label="Birth/Founded Place">{data_customerDetail?.birthFoundedPlace}</DetailText>
-              <DetailText label="Sex">{data_customerDetail?.sex}</DetailText>
-              <DetailText label="Maritial Status">{data_customerDetail?.maritialStatus}</DetailText>
-              <DetailText label="Search Key">{data_customerDetail?.searchKey}</DetailText>
-            </div>
-
-            {/* Account Information */}
-            <div className="text-primary text-xs font-bold uppercase">
-              ACCOUNT INFORMATION
-            </div>
-            <div className="w-full grid grid-cols-4 gap-x-4">
-              <DetailText label="Account Number">{data_accountDetail?.accountSummary?.accountNumber}</DetailText>
-              <DetailText label="Registration Number">{data_accountDetail?.accountSummary?.registrationNumber}</DetailText>
-              <DetailText label="Account Name">{data_accountDetail?.accountSummary?.accountName}</DetailText>
-              <DetailText label="Category">{data_accountDetail?.accountSummary?.category}</DetailText>
-              <DetailText label="SOR">{data_accountDetail?.accountSummary?.sor}</DetailText>
-              <DetailText label="Cost Center">{data_accountDetail?.accountSummary?.costCenter}</DetailText>
-              <DetailText label="Meter Reading Codes">{renderDate(data_accountDetail?.accountSummary?.meterReadingCodes || "")}</DetailText>
-              <DetailText label="Customer Management">{data_accountDetail?.accountSummary?.customerManagement}</DetailText>
-              <DetailText label="Classification Type">{data_accountDetail?.accountSummary?.classificationType}</DetailText>
-              <DetailText label="Segment">{data_accountDetail?.accountSummary?.segment}</DetailText>
-              <DetailText label="Account Group Type">{data_accountDetail?.accountSummary?.accountGroupType}</DetailText>
-              <DetailText label="Premise Address">{data_accountDetail?.accountSummary?.premiseAddress}</DetailText>
-              <DetailText label="Subdistrict">{data_accountDetail?.accountSummary?.subdistrict}</DetailText>
-              <DetailText label="District">{data_accountDetail?.accountSummary?.district}</DetailText>
-              <DetailText label="City">{data_accountDetail?.accountSummary?.city}</DetailText>
-              <DetailText label="Country">{data_accountDetail?.accountSummary?.country}</DetailText>
-              <DetailText label="Longitude">{data_accountDetail?.accountSummary?.longitude}</DetailText>
-              <DetailText label="Latitude">{data_accountDetail?.accountSummary?.latitude}</DetailText>
-              <DetailText label="Status">{data_accountDetail?.accountSummary?.status}</DetailText>    
-            </div>
+        <CardContainer header={"CUSTOMER INFORMATION"}>
+          <div className="w-full grid grid-cols-4 gap-x-5">
+            <DetailText label="Customer Number">{data_customerDetail?.customerNumber}</DetailText>
+            <DetailText label="Identification Type">{data_customerDetail?.identificationType}</DetailText>
+            <DetailText label="Customer Identification Number">{data_customerDetail?.customerIdentificationNumber}</DetailText>
+            <DetailText label="Customer Name">{data_customerDetail?.customerName}</DetailText>
+            <DetailText label="Customer Type">{data_customerDetail?.customerType}</DetailText>
+            <DetailText label="Description">{data_customerDetail?.description}</DetailText>
+            <DetailText label="Birth/Founded Date">{renderDate(data_customerDetail?.birthFoundedDate)}</DetailText>
+            <DetailText label="Birth/Founded Place">{data_customerDetail?.birthFoundedPlace}</DetailText>
+            <DetailText label="Sex">{data_customerDetail?.sex}</DetailText>
+            <DetailText label="Maritial Status">{data_customerDetail?.maritialStatus}</DetailText>
+            <DetailText label="Search Key">{data_customerDetail?.searchKey}</DetailText>
           </div>
-        </BaseContainer>
+        </CardContainer>
+        <CardContainer header={"ACCOUNT INFORMATION"}>
+          <div className="w-full grid grid-cols-4 gap-x-4">
+            <DetailText label="Account Number">{data_accountDetail?.accountSummary?.accountNumber}</DetailText>
+            <DetailText label="Registration Number">{data_accountDetail?.accountSummary?.registrationNumber}</DetailText>
+            <DetailText label="Account Name">{data_accountDetail?.accountSummary?.accountName}</DetailText>
+            <DetailText label="Category">{data_accountDetail?.accountSummary?.category}</DetailText>
+            <DetailText label="SOR">{data_accountDetail?.accountSummary?.sor}</DetailText>
+            <DetailText label="Cost Center">{data_accountDetail?.accountSummary?.costCenter}</DetailText>
+            <DetailText label="Meter Reading Codes">{renderDate(data_accountDetail?.accountSummary?.meterReadingCodes || "")}</DetailText>
+            <DetailText label="Customer Management">{data_accountDetail?.accountSummary?.customerManagement}</DetailText>
+            <DetailText label="Classification Type">{data_accountDetail?.accountSummary?.classificationType}</DetailText>
+            <DetailText label="Segment">{data_accountDetail?.accountSummary?.segment}</DetailText>
+            <DetailText label="Account Group Type">{data_accountDetail?.accountSummary?.accountGroupType}</DetailText>
+            <DetailText label="Premise Address">{data_accountDetail?.accountSummary?.premiseAddress}</DetailText>
+            <DetailText label="Subdistrict">{data_accountDetail?.accountSummary?.subdistrict}</DetailText>
+            <DetailText label="District">{data_accountDetail?.accountSummary?.district}</DetailText>
+            <DetailText label="City">{data_accountDetail?.accountSummary?.city}</DetailText>
+            <DetailText label="Country">{data_accountDetail?.accountSummary?.country}</DetailText>
+            <DetailText label="Longitude">{data_accountDetail?.accountSummary?.longitude}</DetailText>
+            <DetailText label="Latitude">{data_accountDetail?.accountSummary?.latitude}</DetailText>
+            <DetailText label="Status">{data_accountDetail?.accountSummary?.status}</DetailText>    
+          </div>
+        </CardContainer>
 
         <Form
           id="paymentRelationForm"
