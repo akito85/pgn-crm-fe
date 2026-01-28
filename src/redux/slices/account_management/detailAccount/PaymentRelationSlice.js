313d2103a612996363ddcd9c2f1c05e3e31c2894
Jonathan Paskalis <ymjonathangp2@gmail.com>
2026-01-28 13:40:31 +0700

    fix(payment relation): fixed detail page - fixed infinite scroll duplicates bug in attachment table

--- src/redux/slices/account_management/detailAccount/PaymentRelationSlice.js
+++ src/redux/slices/account_management/detailAccount/PaymentRelationSlice.js
@@ -529,13 +529,18 @@ const paymentRelationSlice = createSlice({
       state.loading = false;
       const { result, page, isLoadMore } = action.payload;
 
-      if (isLoadMore)
-        state.list_prDetailAttachment = [
-          ...state.list_paymentRelation,
-          ...result,
-        ];
-      else
-        state.list_prDetailAttachment = result;
+      if (Array.isArray(result)) {
+        const currentIds = new Set(state.list_paymentRelation.map((item) => item.id))
+        const filteredResult = result.filter((resultItem) => !currentIds.has(resultItem.id))
+
+        if (isLoadMore)
+          state.list_prDetailAttachment = [
+            ...state.list_paymentRelation,
+            ...filteredResult,
+          ];
+        else
+          state.list_prDetailAttachment = result;
+      }
 
       state.pagination_prDetailAttachment = {
         totalPages: page?.totalPages || 0,
