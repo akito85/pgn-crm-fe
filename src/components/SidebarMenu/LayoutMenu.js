e4ca700ebea6c7abb1c23f25a77c3a0f57d1f7b8
Jonathan Paskalis <ymjonathangp2@gmail.com>
2026-01-29 17:09:39 +0700

    fix(general): fixed header and main components - fixed layout

--- src/components/SidebarMenu/LayoutMenu.js
+++ src/components/SidebarMenu/LayoutMenu.js
@@ -400,7 +400,7 @@ const LayoutMenu = ({ children }) => {
           </div>
           <SideMenu isCollapsed={collapsed} />
         </Sider>
-        <Layout className="site-layout2">
+        <Layout className="site-layout2 p-4">
           <Header
             className="site-layout-background2"
             style={{
@@ -458,8 +458,6 @@ const LayoutMenu = ({ children }) => {
           </Header>
           <Content
             style={{
-              marginLeft: "20px",
-              marginRight: "20px",
               overflow: "initial",
             }}
           >
