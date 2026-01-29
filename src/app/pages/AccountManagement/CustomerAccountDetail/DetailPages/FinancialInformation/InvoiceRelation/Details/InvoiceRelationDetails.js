27c31f8309e82b8668393e3b7c12fa00952bd027
Jonathan Paskalis <ymjonathangp2@gmail.com>
2026-01-29 17:54:04 +0700

    fix(invoice relation): fixed detail page layout

--- src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/InvoiceRelation/Details/InvoiceRelationDetails.js
+++ src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/InvoiceRelation/Details/InvoiceRelationDetails.js
@@ -19,6 +19,7 @@ import { getDetailInvoiceRelation, getInvoiceRelationAttachment, approveOrReject
 import ModalApproveOrReject from "../../../../../../../../components/Modal/ModalApproveOrReject";
 import { showModalError } from "../../../../../../../../redux/slices/general_slice";
 import NxCardContainer from "../../../../../../../../components/Nx/NxCardContainer";
+import NxBreadCrumb from "../../../../../../../../components/Nx/NxBreadCrumb";
 
 const InvoiceRelationDetails = ({
   type = "standard"
@@ -184,102 +185,103 @@ const InvoiceRelationDetails = ({
   return (
     <LayoutMenu>
       <Spin spinning={isLoading} className={"w-full top-20"}>
-        <BreadCrumb routes={routes} />
-        
-        <NxCardContainer header={"CUSTOMER & ACCOUNT INFORMATION"}>
-          <div className="flex flex-col gap-y-4">
-            <BaseContainer border header={"CUSTOMER INFORMATION"}>
-              <div className="w-full grid grid-cols-4 gap-x-5">
-                <DetailText label="Customer Number">{data_customerDetail?.customerNumber}</DetailText>
-                <DetailText label="Identification Type">{data_customerDetail?.identificationType}</DetailText>
-                <DetailText label="Customer Identification Number">{data_customerDetail?.customerIdentificationNumber}</DetailText>
-                <DetailText label="Customer Name">{data_customerDetail?.customerName}</DetailText>
-                <DetailText label="Customer Type">{data_customerDetail?.customerType}</DetailText>
-                <DetailText label="Description">{data_customerDetail?.description}</DetailText>
-                <DetailText label="Birth/Founded Date">{renderDate(data_customerDetail?.birthFoundedDate || "")}</DetailText>
-                <DetailText label="Birth/Founded Place">{data_customerDetail?.birthFoundedPlace}</DetailText>
-                <DetailText label="Sex">{data_customerDetail?.sex}</DetailText>
-                <DetailText label="Maritial Status">{data_customerDetail?.maritialStatus}</DetailText>
-                <DetailText label="Search Key">{data_customerDetail?.searchKey}</DetailText>
-              </div>
-            </BaseContainer>
-            <BaseContainer border header={"ACCOUNT INFORMATION"}>
-              <div className="w-full grid grid-cols-4 gap-x-4">
-                <DetailText label="Account Number">{data_accountDetail?.accountSummary?.accountNumber}</DetailText>
-                <DetailText label="Registration Number">{data_accountDetail?.accountSummary?.registrationNumber}</DetailText>
-                <DetailText label="Account Name">{data_accountDetail?.accountSummary?.accountName}</DetailText>
-                <DetailText label="Category">{data_accountDetail?.accountSummary?.category}</DetailText>
-                <DetailText label="SOR">{data_accountDetail?.accountSummary?.sor}</DetailText>
-                <DetailText label="Cost Center">{data_accountDetail?.accountSummary?.costCenter}</DetailText>
-                <DetailText label="Meter Reading Codes">{renderDate(data_accountDetail?.meterReadingCodes || "")}</DetailText>
-                <DetailText label="Customer Management">{data_accountDetail?.accountSummary?.customerManagement}</DetailText>
-                <DetailText label="Classification Type">{data_accountDetail?.accountSummary?.classificationType}</DetailText>
-                <DetailText label="Segment">{data_accountDetail?.accountSummary?.segment}</DetailText>
-                <DetailText label="Account Group Type">{data_accountDetail?.accountSummary?.accountGroupType}</DetailText>
-                <DetailText label="Premise Address">{data_accountDetail?.accountSummary?.premiseAddress}</DetailText>
-                <DetailText label="Subdistrict">{data_accountDetail?.accountSummary?.subdistrict}</DetailText>
-                <DetailText label="District">{data_accountDetail?.accountSummary?.district}</DetailText>
-                <DetailText label="City">{data_accountDetail?.accountSummary?.city}</DetailText>
-                <DetailText label="Country">{data_accountDetail?.accountSummary?.country}</DetailText>
-                <DetailText label="Longitude">{data_accountDetail?.accountSummary?.longitude}</DetailText>
-                <DetailText label="Latitude">{data_accountDetail?.accountSummary?.latitude}</DetailText>
-                <DetailText label="Status">{data_accountDetail?.accountSummary?.status}</DetailText>    
-              </div>
-            </BaseContainer>
-          </div>
-        </NxCardContainer>
+        <div className="flex flex-col gap-y-4">
+          <NxBreadCrumb routes={routes} />
+          <NxCardContainer header={"CUSTOMER & ACCOUNT INFORMATION"}>
+            <div className="flex flex-col gap-y-4">
+              <BaseContainer border header={"CUSTOMER INFORMATION"}>
+                <div className="w-full grid grid-cols-4 gap-x-5">
+                  <DetailText label="Customer Number">{data_customerDetail?.customerNumber}</DetailText>
+                  <DetailText label="Identification Type">{data_customerDetail?.identificationType}</DetailText>
+                  <DetailText label="Customer Identification Number">{data_customerDetail?.customerIdentificationNumber}</DetailText>
+                  <DetailText label="Customer Name">{data_customerDetail?.customerName}</DetailText>
+                  <DetailText label="Customer Type">{data_customerDetail?.customerType}</DetailText>
+                  <DetailText label="Description">{data_customerDetail?.description}</DetailText>
+                  <DetailText label="Birth/Founded Date">{renderDate(data_customerDetail?.birthFoundedDate || "")}</DetailText>
+                  <DetailText label="Birth/Founded Place">{data_customerDetail?.birthFoundedPlace}</DetailText>
+                  <DetailText label="Sex">{data_customerDetail?.sex}</DetailText>
+                  <DetailText label="Maritial Status">{data_customerDetail?.maritialStatus}</DetailText>
+                  <DetailText label="Search Key">{data_customerDetail?.searchKey}</DetailText>
+                </div>
+              </BaseContainer>
+              <BaseContainer border header={"ACCOUNT INFORMATION"}>
+                <div className="w-full grid grid-cols-4 gap-x-4">
+                  <DetailText label="Account Number">{data_accountDetail?.accountSummary?.accountNumber}</DetailText>
+                  <DetailText label="Registration Number">{data_accountDetail?.accountSummary?.registrationNumber}</DetailText>
+                  <DetailText label="Account Name">{data_accountDetail?.accountSummary?.accountName}</DetailText>
+                  <DetailText label="Category">{data_accountDetail?.accountSummary?.category}</DetailText>
+                  <DetailText label="SOR">{data_accountDetail?.accountSummary?.sor}</DetailText>
+                  <DetailText label="Cost Center">{data_accountDetail?.accountSummary?.costCenter}</DetailText>
+                  <DetailText label="Meter Reading Codes">{renderDate(data_accountDetail?.meterReadingCodes || "")}</DetailText>
+                  <DetailText label="Customer Management">{data_accountDetail?.accountSummary?.customerManagement}</DetailText>
+                  <DetailText label="Classification Type">{data_accountDetail?.accountSummary?.classificationType}</DetailText>
+                  <DetailText label="Segment">{data_accountDetail?.accountSummary?.segment}</DetailText>
+                  <DetailText label="Account Group Type">{data_accountDetail?.accountSummary?.accountGroupType}</DetailText>
+                  <DetailText label="Premise Address">{data_accountDetail?.accountSummary?.premiseAddress}</DetailText>
+                  <DetailText label="Subdistrict">{data_accountDetail?.accountSummary?.subdistrict}</DetailText>
+                  <DetailText label="District">{data_accountDetail?.accountSummary?.district}</DetailText>
+                  <DetailText label="City">{data_accountDetail?.accountSummary?.city}</DetailText>
+                  <DetailText label="Country">{data_accountDetail?.accountSummary?.country}</DetailText>
+                  <DetailText label="Longitude">{data_accountDetail?.accountSummary?.longitude}</DetailText>
+                  <DetailText label="Latitude">{data_accountDetail?.accountSummary?.latitude}</DetailText>
+                  <DetailText label="Status">{data_accountDetail?.accountSummary?.status}</DetailText>    
+                </div>
+              </BaseContainer>
+            </div>
+          </NxCardContainer>
 
-        <InvoiceRelationDetailTabs
-          dataDetail={detail_invoiceRelation}
-          dataAttachment={data_invoiceRelationAttachment}
-          subjectAccountNumber={data_accountDetail?.accountSummary?.accountNumber}
-          dispatch={dispatch}
-        />
+          <InvoiceRelationDetailTabs
+            dataDetail={detail_invoiceRelation}
+            dataAttachment={data_invoiceRelationAttachment}
+            subjectAccountNumber={data_accountDetail?.accountSummary?.accountNumber}
+            dispatch={dispatch}
+          />
 
-        <NxCardContainer header={"HISTORY LOG INFORMATION"}>
-          <div className="w-full grid grid-cols-5 gap-4">
-            {/* History Log Information */}
-            <DetailText label="Record Id">{detail_invoiceRelation.id}</DetailText>
-            <DetailText label="Created Date">{detail_invoiceRelation.createdDate ? moment(detail_invoiceRelation.createdDate, dateFormatting.meas_date).format(dateFormatting.dateTime) : ""}</DetailText>
-            <DetailText label="Created By">{detail_invoiceRelation.createdBy}</DetailText>
-            <DetailText label="Updated Date">{detail_invoiceRelation.updatedDate ? moment(detail_invoiceRelation.updatedDate, dateFormatting.meas_date).format(dateFormatting.dateTime) : ""}</DetailText>
-            <DetailText label="Updated By">{detail_invoiceRelation.updatedBy}</DetailText>
-          </div>
-        </NxCardContainer>
+          <NxCardContainer header={"HISTORY LOG INFORMATION"}>
+            <div className="w-full grid grid-cols-5 gap-4">
+              {/* History Log Information */}
+              <DetailText label="Record Id">{detail_invoiceRelation.id}</DetailText>
+              <DetailText label="Created Date">{detail_invoiceRelation.createdDate ? moment(detail_invoiceRelation.createdDate, dateFormatting.meas_date).format(dateFormatting.dateTime) : ""}</DetailText>
+              <DetailText label="Created By">{detail_invoiceRelation.createdBy}</DetailText>
+              <DetailText label="Updated Date">{detail_invoiceRelation.updatedDate ? moment(detail_invoiceRelation.updatedDate, dateFormatting.meas_date).format(dateFormatting.dateTime) : ""}</DetailText>
+              <DetailText label="Updated By">{detail_invoiceRelation.updatedBy}</DetailText>
+            </div>
+          </NxCardContainer>
 
-        <div>
-          <div className="flex justify-between">
-            <ButtonComponent
-              type={"submit"}
-              onClick={() => navigate(-1)}
-              icon={
-                <LeftOutlined
-                  style={{
-                    color: "#fff",
-                    fontSize: 24,
-                    justifyItems: "center",
-                  }}
-                />
-              }
-            >
-              Back
-            </ButtonComponent>
-            {isApproval && (
-              <div className={"w-full flex justify-end gap-5"}>
-                <ButtonComponent
-                  type="reject"
-                  onClick={() => handleApprovalModal(true, "reject")}
-                >
-                  Reject
-                </ButtonComponent>
-                <ButtonComponent
-                  type="approve"
-                  onClick={() => handleApprovalModal(true, "approve")}
-                >
-                  Approve
-                </ButtonComponent>
-              </div>
-            )}
+          <div>
+            <div className="flex justify-between">
+              <ButtonComponent
+                type={"submit"}
+                onClick={() => navigate(-1)}
+                icon={
+                  <LeftOutlined
+                    style={{
+                      color: "#fff",
+                      fontSize: 24,
+                      justifyItems: "center",
+                    }}
+                  />
+                }
+              >
+                Back
+              </ButtonComponent>
+              {isApproval && (
+                <div className={"w-full flex justify-end gap-5"}>
+                  <ButtonComponent
+                    type="reject"
+                    onClick={() => handleApprovalModal(true, "reject")}
+                  >
+                    Reject
+                  </ButtonComponent>
+                  <ButtonComponent
+                    type="approve"
+                    onClick={() => handleApprovalModal(true, "approve")}
+                  >
+                    Approve
+                  </ButtonComponent>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </Spin>
