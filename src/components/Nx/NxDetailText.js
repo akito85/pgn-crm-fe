1dc7f24c3cb2a73f93a309e1f4e1fbaffea56a61
Jonathan Paskalis <ymjonathangp2@gmail.com>
2026-02-02 15:31:16 +0700

    fix(general): fixed and duplicate detail text comp

---
+++ src/components/Nx/NxDetailText.js
@@ -0,0 +1,10 @@
+const NxDetailText = ({ label, children, className = "", classTextAdditional ="" }) => {
+  return (
+    <div className={className}>
+      <label className="text-xs font-semibold">{label}</label>
+      <p className={`mb-0 text-xs ${classTextAdditional}`}>{children}</p>
+    </div>
+  );
+};
+
+export default NxDetailText;
