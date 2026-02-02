10619266df70c074f44c2a22d6758b535113b65f
Jonathan Paskalis <ymjonathangp2@gmail.com>
2026-02-02 17:48:55 +0700

    fix(general): fixed Nx card container - match new design

--- src/components/Nx/NxCardContainer.js
+++ src/components/Nx/NxCardContainer.js
@@ -1,4 +1,4 @@
-const NxCardContainer = ({ header, subHeader, children, type, element, className, hideChildren = false, withoutTopPadding = false }) => {
+const NxCardContainer = ({ header, subHeader, children, type, element, className, hideChildren = false, withoutPadding = false }) => {
   return (
     <div className={`drop-shadow-lg bg-white rounded-lg w-full ${className}`}>
       {type === "profile" || type === "tab" ? (
@@ -7,15 +7,14 @@ const NxCardContainer = ({ header, subHeader, children, type, element, className
         </>
       ) : (
         <div
-          className="flex flex-col bg-[#F9F9F9] rounded-t-lg uppercase"
-          style={{ borderBottom: "1px solid #BDBDBD", padding: "16px" }}
+          className="flex flex-col bg-[#F9F9F9] rounded-t-lg uppercase p-4 border-0 border-b border-solid border-[#C8CDD4]"
         >
           <div className="text-[16px] text-primary">{header}</div>
           <div className="text-primary text-sm">{subHeader}</div>
         </div>
       )}
-      {type === "tabs" && <div className={`p-4 ${withoutTopPadding ? "pt-0" : ""}`}>{element}</div>}
-      {!hideChildren && <div className={`p-4 ${withoutTopPadding ? "pt-0" : ""}`}>{children}</div>}
+      {type === "tabs" && <div className={`${withoutPadding ? "" : "p-4"}`}>{element}</div>}
+      {!hideChildren && <div className={`${withoutPadding ? "" : "p-4"}`}>{children}</div>}
     </div>
   );
 };
