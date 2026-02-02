ff09436cf730ddbf65b931e72b7468bdc4aaf883
Jonathan Paskalis <ymjonathangp2@gmail.com>
2026-02-02 16:30:13 +0700

    feat(general): added Nx tabs component

---
+++ src/components/Nx/NxTabs.js
@@ -0,0 +1,20 @@
+import { Tabs } from "antd"
+
+const NxTabs = ({
+  items = [],
+  onChange = () => {},
+  activeKey = "",
+  forModal = false, // Set to true for usage inside a modal
+  className = "",
+  ...props
+}) => (
+  <Tabs
+    items={items}
+    onChange={onChange}
+    activeKey={activeKey}
+    className={`[&_.ant-tabs-tab]:text-[12px] [&_.ant-tabs-tab]:py-4 [&_.ant-tabs-nav]:!mb-0 ${forModal ? "[&_.ant-tabs-nav-list]:px-4" : ""}  [&_.ant-tabs-nav::before]:!border-b-[#C8CDD4] [&_.ant-tabs-nav]:pt-0 [&_.ant-tabs-content]:p-4 ${className}`}
+    {...props}
+  />
+)
+
+export default NxTabs;
