d972eeb22cef78d4b27dde09c3508f773f82279d
Jonathan Paskalis <ymjonathangp2@gmail.com>
2026-02-02 16:36:04 +0700

    feat(general): devd Nx detail text component - added default class

--- src/components/Nx/NxDetailText.js
+++ src/components/Nx/NxDetailText.js
@@ -1,6 +1,6 @@
 const NxDetailText = ({ label, children, className = "", classTextAdditional ="" }) => {
   return (
-    <div className={className}>
+    <div className={`flex flex-col gap-y-2 ${className}`}>
       <label className="text-xs font-semibold">{label}</label>
       <p className={`mb-0 text-xs ${classTextAdditional}`}>{children}</p>
     </div>
