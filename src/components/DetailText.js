e92c4ba35d956e621ae7053f97ad7c212631df33
Jonathan Paskalis <ymjonathangp2@gmail.com>
2026-02-02 15:31:16 +0700

    fix(general): fixed and duplicate detail text comp

--- src/components/DetailText.js
+++ src/components/DetailText.js
@@ -2,7 +2,7 @@ const DetailText = ({ label, children, className = "", classTextAdditional ="" }
   return (
     <div className={className}>
       <label className="text-xs font-semibold">{label}</label>
-      <p className={`mb-0 text-xs ${classTextAdditional}`}>{children}</p>
+      <p className={`text-xs ${classTextAdditional}`}>{children}</p>
     </div>
   );
 };
