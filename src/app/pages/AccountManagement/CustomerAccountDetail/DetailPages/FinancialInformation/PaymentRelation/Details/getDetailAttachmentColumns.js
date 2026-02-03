8ba5b22915500a3ceaf3cfd6ff5719c8e6360ab7
Jonathan Paskalis <ymjonathangp2@gmail.com>
2026-02-03 12:13:44 +0700

    fix(payment relation): fixed detail page - fixed fixed coloumns

--- src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/PaymentRelation/Details/getDetailAttachmentColumns.js
+++ src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/PaymentRelation/Details/getDetailAttachmentColumns.js
@@ -35,6 +35,7 @@ const getDetailAttachmentColumns = (
     ),
   },
   {
+    key: "filename",
     title: "FILE NAME",
     dataIndex: "fileName",
     width: 300,
@@ -51,6 +52,7 @@ const getDetailAttachmentColumns = (
     ),
   },
   {
+    key: "fileSize",
     title: "FILE SIZE",
     dataIndex: "fileSize",
     width: 100,
