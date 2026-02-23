87ce491ff827c2cf7f0158c8718f3753b498fc7a
Jonathan Paskalis <ymjonathangp2@gmail.com>
2026-02-23 14:12:29 +0700

    fix(invoice relation): fix statusApproval render - add map for "WAITING_FOR_APPROVAL" value

--- src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/InvoiceRelation/getInvoiceRelationColumns.js
+++ src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/InvoiceRelation/getInvoiceRelationColumns.js
@@ -129,7 +129,8 @@ const getInvoiceRelationColumns = (
         "waitingApproval": "Waiting Approval",
         "pending": "Pending",
         "rejected": "Rejected",
-        "WAITING_APPROVAL": "Waiting Approval"
+        "WAITING_APPROVAL": "Waiting Approval",
+        "WAITING_FOR_APPROVAL": "Waiting Approval"
       };
       return (
         <div className="flex justify-center">
