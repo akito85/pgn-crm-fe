a7a8f1eec05709fdb53b707f48d7e9bfc106cb47
Jonathan Paskalis <ymjonathangp2@gmail.com>
2026-02-26 13:27:56 +0700

    refactor(invoice relation): - change moment to NxDate.formatDate

--- src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/InvoiceRelation/Details/InvoiceRelationDetailInfo.js
+++ src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/InvoiceRelation/Details/InvoiceRelationDetailInfo.js
@@ -1,25 +1,35 @@
-import moment from "moment";
-import { dateFormatting } from "../../../../../../../../utils";
 import NxDetailText from "../../../../../../../../components/Nx/NxDetailText";
 import NxBaseContainer from "../../../../../../../../components/Nx/NxBaseContainer";
+import NxDate from "../../../../../../../../components/Nx/NxDatePicker";
 
 const InvoiceRelationDetailInfo = ({
   subjectAccountNumber,
   dataDetail = {},
 }) => {
+  const {
+    accountNumber,
+    accountName,
+    relatedAccountNumber,
+    relatedAccountName,
+    startDate,
+    endDate,
+    status,
+    description,
+  } = dataDetail;
+
   return (
     <NxBaseContainer border>
       <div className="flex flex-col gap-y-4">
         <div className="w-full grid grid-cols-3 gap-4">
           {/* Invoice Relation Information */}
-          <NxDetailText label="Account Number">{subjectAccountNumber === dataDetail?.accountNumber ? dataDetail?.relatedAccountNumber : dataDetail?.accountNumber}</NxDetailText>
-          <NxDetailText label="Account Name">{subjectAccountNumber === dataDetail?.accountNumber ? dataDetail?.relatedAccountName : dataDetail?.accountName}</NxDetailText>
-          <NxDetailText label="Start Date">{dataDetail?.startDate ? moment(dataDetail.startDate, dateFormatting.f_date).format(dateFormatting.date) : ""}</NxDetailText>
-          <NxDetailText label="End Date">{dataDetail?.endDate ? moment(dataDetail.endDate, dateFormatting.f_date).format(dateFormatting.date) : ""}</NxDetailText>
-          <NxDetailText label="Status">{dataDetail?.status}</NxDetailText>
+          <NxDetailText label="Account Number">{subjectAccountNumber === accountNumber ? relatedAccountNumber : accountNumber}</NxDetailText>
+          <NxDetailText label="Account Name">{subjectAccountNumber === accountNumber ? relatedAccountName : accountName}</NxDetailText>
+          <NxDetailText label="Start Date">{NxDate.formatDate(startDate)}</NxDetailText>
+          <NxDetailText label="End Date">{NxDate.formatDate(endDate)}</NxDetailText>
+          <NxDetailText label="Status">{status}</NxDetailText>
         </div>
         <div className="w-full">
-          <NxDetailText label="Description">{dataDetail?.description}</NxDetailText>
+          <NxDetailText label="Description">{description}</NxDetailText>
         </div>
       </div>
     </NxBaseContainer>
--- src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/InvoiceRelation/getInvoiceRelationColumns.js
+++ src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/InvoiceRelation/getInvoiceRelationColumns.js
@@ -1,7 +1,7 @@
-import moment from "moment";
-import { dateFormatting, toTitleCase } from "../../../../../../../utils";
+import { toTitleCase } from "../../../../../../../utils";
 import { getColumnSearchPropsUseFilteredValue } from "../../../../../../../utils/getColumnSearchProps";
 import StatusComponent from "../../../../../../../components/StatusComponent";
+import NxDate from "../../../../../../../components/Nx/NxDatePicker";
 
 const getInvoiceRelationColumns = (
   search,
@@ -86,7 +86,7 @@ const getInvoiceRelationColumns = (
       handleSearch,
       true
     ),
-    render: (startDate) => startDate ? moment(startDate, "DD-MM-YYYY").format(dateFormatting.date) : "",
+    render: (startDate) => NxDate.formatDate(startDate),
   },
   {
     key: "endDate",
@@ -104,7 +104,7 @@ const getInvoiceRelationColumns = (
       handleSearch,
       true
     ),
-    render: (endDate) => endDate ? moment(endDate, "DD-MM-YYYY").format(dateFormatting.date) : "",
+    render: (endDate) => NxDate.formatDate(endDate),
   },
   {
     key: "statusApproval",
