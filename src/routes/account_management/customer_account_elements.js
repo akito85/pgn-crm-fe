42e97acd5e95a790e2acd76a30bea41d56613bb8
Jonathan Paskalis <ymjonathangp2@gmail.com>
2026-03-06 11:17:00 +0700

    fix(relationship): routes and elements

--- src/routes/account_management/customer_account_elements.js
+++ src/routes/account_management/customer_account_elements.js
@@ -153,9 +153,9 @@ export const ACCOUNT_MANAGEMENT_ELEMENTS = {
   DETAIL_RELATIONSHIP_PAGE: <RelationshipDetails accountType="standard" />,
 
   //RELATIONSHIP ACCOUNT ONE TIME
-  CREATE_RELATIONSHIP_PAGE: <CreateUpdateRelationship accountType="oneTime" formType="create" />,
-  UPDATE_RELATIONSHIP_PAGE: <CreateUpdateRelationship accountType="oneTime" formType="update" />,
-  DETAIL_RELATIONSHIP_PAGE: <RelationshipDetails accountType="oneTime" />,
+  CREATE_RELATIONSHIP_ONETIME_PAGE: <CreateUpdateRelationship accountType="oneTime" formType="create" />,
+  UPDATE_RELATIONSHIP_ONETIME_PAGE: <CreateUpdateRelationship accountType="oneTime" formType="update" />,
+  DETAIL_RELATIONSHIP_ONETIME_PAGE: <RelationshipDetails accountType="oneTime" />,
 
   //SA-APPROVE-REJECT-TOS
   APPROVE_OR_REJECT_TOS_PAGE: <ApproveOrRejectTOS />,
