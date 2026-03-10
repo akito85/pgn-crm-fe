c00817401688097b4c43e99c90a25d3a076cfe2e
Jonathan Paskalis <ymjonathangp2@gmail.com>
2026-03-10 14:28:44 +0700

    feat(gas-deposit): implemented slice

--- src/redux/stores/store.js
+++ src/redux/stores/store.js
@@ -127,6 +127,7 @@ import transferToCustomerReducer from "../slices/receipt_collection/transferToCu
 import restructureReducer from "../slices/receipt_collection/restructure";
 import notificationsReducer from "../slices/notifications";
 import multiDestinationReducer from "../slices/account_management/detailAccount/MultiDestinationSlice"
+import gasDepositReducer from "../slices/account_management/detailAccount/GasDepositSlice"
 import deductionReducer from "../slices/receipt_collection/deduction";
 import gapuraManagementReducer from "../slices/receipt_collection/gapuraManagement";
 import historyWarrantyReducer from "../slices/receipt_collection/historyWarranty";
@@ -213,7 +214,9 @@ const reducer = combineReducers({
   accountEquipment: equpmentReducer,
   accountPromo: accountPromoReducer,
   relationship: relationshipReducer,
-
+  multiDestination: multiDestinationReducer,
+  gasDeposit: gasDepositReducer,
+  
   //Account Management ( Customer )
   customerAccount: customerAccountReducer,
 
@@ -226,7 +229,7 @@ const reducer = combineReducers({
   accounting_rules: accountingRulesReducer,
   tax_implication: taxImplicationReducer,
   assets: assetsReducer,
-
+  
   // Global reducer
   globalProp: globalPropReducer,
 
@@ -319,7 +322,6 @@ const reducer = combineReducers({
 
   // notifications
   notifications: notificationsReducer,
-  multiDestination: multiDestinationReducer,
 });
 
 // add throttle middlewares
