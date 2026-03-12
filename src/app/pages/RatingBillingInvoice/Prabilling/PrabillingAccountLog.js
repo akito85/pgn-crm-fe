364a9abc68360dfecebf1a276ec0c89cedad279f
ramabhagadhara <ramabhagadhara08@gmail.com>
2026-03-13 00:07:07 +0700

    chore:fix button in detail monitoring usage

--- src/app/pages/RatingBillingInvoice/Prabilling/PrabillingAccountLog.js
+++ src/app/pages/RatingBillingInvoice/Prabilling/PrabillingAccountLog.js
@@ -149,7 +149,7 @@ const PrabillingAccountLog = ({ data, tabHeader }) => {
         ),
         render: (text) => {
           const formattedDate = text
-            ? moment(text).format("DD MMM YYYY")
+           ? moment(text).format("DD MMM YYYY HH:mm:ss")
             : "-";
           return renderDateColumn(
             "createdDate",
