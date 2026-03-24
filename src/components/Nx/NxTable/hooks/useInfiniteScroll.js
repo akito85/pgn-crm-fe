09c261b880b26ddc9b25775667673cfafae5e5d6
Vitrio Wibisono <vitrio@noxus.co.id>
2026-03-24 07:54:41 +0700

    fix(nxtable): remove unused useLayoutEffect import from useInfiniteScroll

--- src/components/Nx/NxTable/hooks/useInfiniteScroll.js
+++ src/components/Nx/NxTable/hooks/useInfiniteScroll.js
@@ -1,5 +1,5 @@
 // src/components/Nx/NxTable/hooks/useInfiniteScroll.js
-import React, { useState, useEffect, useLayoutEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { SCROLL_THRESHOLD_PX } from '../constants';
 
 const useInfiniteScroll = ({
