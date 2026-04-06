68b1b10c13fe32073f367db68687eed88f2e258a
Jonathan Paskalis <ymjonathangp2@gmail.com>
2026-04-06 11:33:37 +0700

    fix(gas deposit): route component - fixed module type props

--- src/routes/account_management/customer_account_elements.js
+++ src/routes/account_management/customer_account_elements.js
@@ -148,12 +148,12 @@ export const ACCOUNT_MANAGEMENT_ELEMENTS = {
   EXPIRE_GAS_DEPOSIT_SA_PAGE: "/account-management/gas-deposit/expire",
   
   // Gas Deposit Account Standard
-  VIEW_DETAIL_GAS_DEPOSIT_PAGE: <GasDepositDetail moduleType="sa" accountType="standard" />,
+  VIEW_DETAIL_GAS_DEPOSIT_PAGE: <GasDepositDetail moduleType="ua" accountType="standard" />,
   RECALCULATE_GAS_DEPOSIT_PAGE: "/account-management/account-standard/gas-deposit/recalculate",
   EXPIRE_GAS_DEPOSIT_PAGE: "/account-management/account-standard/gas-deposit/expire",
 
   // Gas Deposit Account One Time
-  VIEW_DETAIL_GAS_DEPOSIT_ONETIME_PAGE: <GasDepositDetail moduleType="sa" accountType="oneTime" />,
+  VIEW_DETAIL_GAS_DEPOSIT_ONETIME_PAGE: <GasDepositDetail moduleType="ua" accountType="oneTime" />,
   RECALCULATE_GAS_DEPOSIT_ONETIME_PAGE: "/account-management/account-onetime/gas-deposit/recalculate",
   EXPIRE_GAS_DEPOSIT_ONETIME_PAGE: "/account-management/account-onetime/gas-deposit/expire",
 
