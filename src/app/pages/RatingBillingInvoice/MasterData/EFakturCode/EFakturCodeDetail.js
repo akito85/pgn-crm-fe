2a7ff2ff8a9e1aef5361984d4746e8c211d1440c
tegarfaris <tegarfarisn@gmail.com>
2026-04-07 09:26:06 +0700

    fix(md-efakturcode): fix efakturcode

--- src/app/pages/RatingBillingInvoice/MasterData/EFakturCode/EFakturCodeDetail.js
+++ src/app/pages/RatingBillingInvoice/MasterData/EFakturCode/EFakturCodeDetail.js
@@ -30,7 +30,7 @@ import ModalApproveOrReject from "../../../../../components/Modal/ModalApproveOr
 const EFakturCodeDetail = () => {
   // Selector
   const { loading, data_detail } = useSelector(
-    (state) => state.masterEfakturCode
+    (state) => state.masterEfakturCode,
   );
 
   // Declaration
@@ -82,15 +82,15 @@ const EFakturCodeDetail = () => {
       // Data Additional Code
       const mappedAdditionalCode = additionalCodes.map((item, index) => ({
         id: item.additionalId || index,
-        code: item.code || "-",
-        description: item.description || "-",
+        code: item.code || "",
+        description: item.description || "",
         startDate: item.startDate || null,
         endDate: item.endDate || null,
-        status: item.status || "Inactive",
+        status: item.status,
         createdDate: item.createdDate || null,
-        createdBy: item.createdBy || "-",
+        createdBy: item.createdBy || "",
         updatedDate: item.updatedDate || null,
-        updatedBy: item.updatedBy || "-",
+        updatedBy: item.updatedBy || "",
       }));
 
       // Data Attachment Information
@@ -126,6 +126,8 @@ const EFakturCodeDetail = () => {
       setDataDetail({
         code: fakturCode?.einvoiceCode || "-",
         description: fakturCode?.description || "-",
+        status: fakturCode?.status || null,
+        statusApproval: fakturCode?.statusApproval || null,
       });
 
       setAdditionalCodeList(mappedAdditionalCode);
@@ -214,7 +216,7 @@ const EFakturCodeDetail = () => {
     setModalConfirm(false);
     const data = {
       remark: res.remark,
-      action: approveOrReject.toUpperCase(),
+      action: approveOrReject.startsWith("Approve") ? "APPROVE" : approveOrReject.toUpperCase(),
       approvalId: bodyApproval.tAppId,
     };
     dispatch(
@@ -226,7 +228,7 @@ const EFakturCodeDetail = () => {
         : approveRejectEfakturCode({
             id: id,
             body: data,
-          })
+          }),
     )
       .unwrap()
       .then(() => {
@@ -265,7 +267,7 @@ const EFakturCodeDetail = () => {
                   <DetailText label={"Requested Date"}>
                     {bodyApproval.approvalDetail?.requestedDate
                       ? moment(
-                          bodyApproval.approvalDetail.requestedDate
+                          bodyApproval.approvalDetail.requestedDate,
                         ).format(dateFormatting.date)
                       : "-"}
                   </DetailText>
@@ -282,20 +284,8 @@ const EFakturCodeDetail = () => {
           {layout(valuePage)}
         </div>
 
-        <div className="flex mt-[30px]">
-          <ButtonComponent
-            type={"submit"}
-            onClick={() => navigate(-1)}
-            icon={
-              <LeftOutlined
-                style={{
-                  color: "#fff",
-                  fontSize: 24,
-                  justifyItems: "center",
-                }}
-              />
-            }
-          >
+        <div className="flex">
+          <ButtonComponent type={"submit"} onClick={() => navigate(-1)}>
             Back
           </ButtonComponent>
 
@@ -314,10 +304,16 @@ const EFakturCodeDetail = () => {
                 type="approve"
                 onClick={() => {
                   setModalConfirm(true);
-                  setApproveOrReject("Approve");
+                  setApproveOrReject(
+                    bodyApproval.approvalType === "INACTIVE_FAKTUR_CODE"
+                      ? "Approve Inactive"
+                      : "Approve",
+                  );
                 }}
               >
-                Approve
+                {bodyApproval.approvalType === "INACTIVE_FAKTUR_CODE"
+                  ? "Approve Inactive"
+                  : "Approve"}
               </ButtonComponent>
             </div>
           ) : null}
--- src/app/pages/RatingBillingInvoice/MasterData/EFakturCode/detail/EFakturCodeSection.js
+++ src/app/pages/RatingBillingInvoice/MasterData/EFakturCode/detail/EFakturCodeSection.js
@@ -1,5 +1,6 @@
 import CardContainer from "../../../../../../components/CardContainer";
 import DetailText from "../../../../../../components/DetailText";
+import StatusComponent from "../../../../../../components/StatusComponent";
 
 const EFakturCodeSection = ({ dataEfakturCode = {} }) => {
   return (
@@ -8,7 +9,28 @@ const EFakturCodeSection = ({ dataEfakturCode = {} }) => {
       <CardContainer header={"Efaktur Code Information"}>
         <div className="w-full grid grid-cols-4 gap-3">
           <DetailText label={"Code"}>{dataEfakturCode?.code || "-"}</DetailText>
-          <div className="col-span-3">
+          <DetailText label={"Status"}>
+            {dataEfakturCode?.status ? (
+              <StatusComponent colour={dataEfakturCode.status} size="small">
+                {dataEfakturCode.status}
+              </StatusComponent>
+            ) : (
+              "-"
+            )}
+          </DetailText>
+          <DetailText label={"Status Approval"}>
+            {dataEfakturCode?.statusApproval ? (
+              <StatusComponent
+                colour={dataEfakturCode.statusApproval}
+                size="small"
+              >
+                {dataEfakturCode.statusApproval}
+              </StatusComponent>
+            ) : (
+              "-"
+            )}
+          </DetailText>
+          <div className="col-span-4">
             <DetailText label={"Description"}>
               {dataEfakturCode?.description || "-"}
             </DetailText>
