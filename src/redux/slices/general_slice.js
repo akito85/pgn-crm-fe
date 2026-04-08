1aa660e790d9bc867b4516a199e69557c828f5fc
Jonathan Paskalis <ymjonathangp2@gmail.com>
2026-04-08 08:55:16 +0700

    feat(general): validation thunk - added conditionals for recalculate and expire

--- src/redux/slices/general_slice.js
+++ src/redux/slices/general_slice.js
@@ -102,7 +102,7 @@ export const validateCreateUpdate = createAsyncThunk(
         validateError({
           error: errorBody(
             errorCode(error),
-            type === "update" ? "updated" : "created",
+            type === "update" ? "updated" : type === "recalculate" ? "recalculated" : type === "expire" ? "expired" : "created",
             errorMessage(error),
             error?.response?.data?.data || null
           ),
