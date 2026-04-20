4cd0e028a0f8cfc5450bcfe99e2de6482bdebde0
Jonathan Paskalis <ymjonathangp2@gmail.com>
2026-04-20 15:45:16 +0700

    fix(invoice relation): detail page - added default back button if isApproval is false

--- src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/InvoiceRelation/Details/InvoiceRelationDetail.js
+++ src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/FinancialInformation/InvoiceRelation/Details/InvoiceRelationDetail.js
@@ -241,36 +241,34 @@ const InvoiceRelationDetail = ({
             </NxBaseContainer>
           </NxCardContainer>
 
-          {isApproval && (
-            <NxBaseContainer border>
-              <div className="flex justify-between">
-                <Button
-                  type={"menu"}
-                  onClick={() => navigate(-1)}
-                >
-                  Cancel
-                </Button>
-                <div className={"w-full flex justify-end gap-5"}>
-                  <Button
-                    type="reject"
-                    icon={<SVGIcon width={14} height={14} name="IconSquareX" />}
-                    className="flex-row-reverse"
-                    onClick={() => handleApprovalModal(true, "reject")}
-                  >
-                    Reject
+          <NxBaseContainer border>
+            <div className="flex justify-between">
+              {isApproval ? (
+                <>
+                  <Button type={"menu"} onClick={() => navigate(-1)}>
+                    Cancel
                   </Button>
-                  <Button
-                    type="approve"
-                    icon={<SVGIcon width={14} height={14} name="IconSquareCheck" />}
-                    className="flex-row-reverse"
-                    onClick={() => handleApprovalModal(true, "approve")}
-                  >
-                    Approve
-                  </Button>
-                </div>
-              </div>
-            </NxBaseContainer>
-          )}
+                  <div className={"w-full flex justify-end gap-x-2"}>
+                    <Button
+                      type="reject"
+                      onClick={() => handleApprovalModal(true, "reject")}
+                    >
+                      Reject
+                    </Button>
+                    <Button
+                      type="approve"
+                      onClick={() => handleApprovalModal(true, "approve")}
+                    >
+                      Approve
+                    </Button>
+                  </div>
+                </>
+              ) :
+              <Button type={"menu"} icon={<SVGIcon name="IconChevronLeft" width={14} />} onClick={() => navigate(-1)}>
+                Back      
+              </Button>}
+            </div>
+          </NxBaseContainer>
         </div>
       </Spin>
       <NxApproveOrRejectModal
