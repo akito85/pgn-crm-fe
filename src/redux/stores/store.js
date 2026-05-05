de6e201677fc62b21e8b7775358374c34a6cb71a
Vitrio Wibisono <vitrio@noxus.co.id>
2026-05-05 11:25:30 +0700

    feat(job-mgmt): handler registry dropdown in CreateJobPage
    Add handlerRegistrySlice fetching GET /v1/api/handlers/grouped.
    Replace free-text handler input with registry-backed Select for
    SCRIPT and CUSTOM_HANDLER execute types. Falls back to text input
    if registry is unavailable or empty.

--- src/redux/stores/store.js
+++ src/redux/stores/store.js
@@ -150,6 +150,7 @@ import jobGroupReducer from "../slices/job_management/jobGroupSlice";
 import jobExecutionReducer from "../slices/job_management/jobExecutionSlice";
 import oracleMetadataReducer from "../slices/job_management/oracleMetadataSlice";
 import taskQueueReducer from "../slices/job_management/taskQueueSlice";
+import handlerRegistryReducer from "../slices/job_management/handlerRegistrySlice";
 import { jobApiSlice } from "../slices/job_management/jobApiSlice";
 import { jobGroupApiSlice } from "../slices/job_management/jobGroupApiSlice";
 
@@ -159,6 +160,7 @@ const reducer = combineReducers({
   jobExecution: jobExecutionReducer,
   oracleMetadata: oracleMetadataReducer,
   taskQueue: taskQueueReducer,
+  handlerRegistry: handlerRegistryReducer,
   auth: authReducer,
   message: messageReducer,
   entity: entityReducer,
