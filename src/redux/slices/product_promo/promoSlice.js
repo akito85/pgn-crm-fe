1626cecc9afb2325b5c580120d2eb1445978b5da
alvin <alvinyr32@gmail.com>
2026-05-06 15:51:59 +0700

    fix(promo): add loading state for create and update promo actions

--- src/redux/slices/product_promo/promoSlice.js
+++ src/redux/slices/product_promo/promoSlice.js
@@ -24,6 +24,7 @@ const initialState = {
   data_promoDiscountDetailDraft: [],
   data_listAttachment: [],
   loading: false,
+  loading_createUpdatePromo: false,
   isFailed: false,
   isSuccess: false,
   message: "",
@@ -1691,6 +1692,26 @@ const promoSlice = createSlice({
     [getTieringList.rejected]: (state, action) => {
       state.loading = false;
       state.data_tiering = action.payload;
+    },
+
+    [createPromo.pending]: (state) => {
+      state.loading_createUpdatePromo = true;
+    },
+    [createPromo.fulfilled]: (state) => {
+      state.loading_createUpdatePromo = false;
+    },
+    [createPromo.rejected]: (state) => {
+      state.loading_createUpdatePromo = false;
+    },
+
+    [updatePromo.pending]: (state) => {
+      state.loading_createUpdatePromo = true;
+    },
+    [updatePromo.fulfilled]: (state) => {
+      state.loading_createUpdatePromo = false;
+    },
+    [updatePromo.rejected]: (state) => {
+      state.loading_createUpdatePromo = false;
     },
   },
 });
