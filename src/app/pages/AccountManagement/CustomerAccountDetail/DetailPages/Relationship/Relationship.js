371d4c927ec3bd84aef9bc6750bd5ec08d6cf1ad
Jonathan Paskalis <ymjonathangp2@gmail.com>
2026-05-06 16:38:54 +0700

    feat(general): NxActivateInactivateModal - changed NxInactivateModal to a more general NxActivateInactivateModal

--- src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/Relationship/Relationship.js
+++ src/app/pages/AccountManagement/CustomerAccountDetail/DetailPages/Relationship/Relationship.js
@@ -8,7 +8,7 @@ import {
   getRelationshipApprovalHierarchies,
   getRelationshipApprovalHierarchy,
 } from "../../../../../../redux/slices/account_management/detailAccount/relationshipSlice";
-import NxInactivateModal from "../../../../../../components/Nx/NxInactivateModal";
+import NxActivateInactivateModal from "../../../../../../components/Nx/NxActivateInactivateModal";
 import NxHistoryModal from "../../../../../../components/Nx/NxHistoryModal";
 import NxCardContainer from "../../../../../../components/Nx/NxCardContainer";
 import { getGrantedAccessAccount } from "../../../../../../redux/slices/account_management/accountManagement";
@@ -158,7 +158,7 @@ const Relationship = ({
         />
 
         {/* Inactivate Modal */}
-        <NxInactivateModal
+        <NxActivateInactivateModal
           isOpen={showInactiveModal}
           header={"INACTIVATE"}
           handleCloseModal={() => handleInactivateModal(false)}
