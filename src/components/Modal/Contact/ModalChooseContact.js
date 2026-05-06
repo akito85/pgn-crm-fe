04d024a054a6fc5102212b1b494863c8bff91bbb
Jonathan Paskalis <ymjonathangp2@gmail.com>
2026-05-06 15:20:02 +0700

    fix(service request): create page - fixed choose contact modal design and feature to match Figma

--- src/components/Modal/Contact/ModalChooseContact.js
+++ src/components/Modal/Contact/ModalChooseContact.js
@@ -2,9 +2,11 @@ import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import ModalCustom from "../ModalCustom";
 import ButtonComponent from "../../ButtonComponent";
 import { getColumnSearchProps } from "../../../utils/getColumnSearchProps";
-import { Tooltip } from "antd";
+import { Button, Tooltip } from "antd";
 import { PlusCircleOutlined } from "@ant-design/icons";
 import NxTable from "../../Nx/NxTable";
+import NxModal from "../../Nx/NxModal";
+import ContactDetailExpandTable from "./ContactDetailExpandTable";
 
 const ModalChooseContact = ({
   isOpen,
@@ -75,14 +77,17 @@ const ModalChooseContact = ({
   const columns = useMemo(() => {
     return [
       {
+        key: "no",
         title: "NO",
-        width: 60,
+        width: 50,
         align: "center",
         render: (text, object, index) => index + 1,
       },
       {
+        key: "contactName",
         title: "CONTACT NAME",
         dataIndex: "contactName",
+        width: 240,
         sorter: true,
         ...getColumnSearchProps(
           "contactName",
@@ -94,8 +99,10 @@ const ModalChooseContact = ({
         ),
       },
       {
+        key: "jobName",
         title: "JOB TITLE",
         dataIndex: "jobName",
+        width: 240,
         sorter: true,
         align: "center",
         ...getColumnSearchProps(
@@ -111,6 +118,7 @@ const ModalChooseContact = ({
       {
         title: "POSITION",
         dataIndex: "positionName",
+        width: 240,
         sorter: true,
         align: "center",
         ...getColumnSearchProps(
@@ -126,6 +134,7 @@ const ModalChooseContact = ({
       {
         title: "SOURCE",
         dataIndex: "source",
+        width: 240,
         sorter: true,
         align: "center",
         ...getColumnSearchProps(
@@ -158,46 +167,6 @@ const ModalChooseContact = ({
     ];
   }, [handleChooseContact, handleSearch, searchText, searchedColumn]);
 
-  // expand row render
-  const expandedRowRender = (record) => {
-    const column = [
-      {
-        title: "NO",
-        align: "center",
-        width: 60,
-        render: (text, object, index) => index + 1,
-      },
-      {
-        title: "TYPE",
-        dataIndex: "typeName",
-        width: 250,
-      },
-      {
-        title: "INPUT TYPE",
-        dataIndex: "inputTypeName",
-      },
-      {
-        title: "VALUE",
-        dataIndex: "fullValue",
-      },
-    ];
-
-    return (
-      <div className="pl-6 py-2">
-        <NxTable
-          idTable={`table-contact-detail-expand-${record.key}`}
-          dataSource={record?.contactDetails}
-          columns={column}
-          useSelect={false}
-          usePagination={false}
-          showAdvanceSearch={false}
-          showSearchBar={false}
-          tableScrolled={{ x: "max-content" }}
-        />
-      </div>
-    );
-  };
-
   const onSort = (_, __, sortInfo) => {
     const dataSort =
       sortInfo.order !== undefined
@@ -223,21 +192,18 @@ const ModalChooseContact = ({
   }, [handleCancelModalChoose]);
 
   return (
-    <ModalCustom
+    <NxModal
       isOpen={isOpen}
-      type="confirmation"
-      header={"Choose Contact"}
+      title={"Choose Contact"}
       handleCancel={handleClose}
       width={1200}
       footer={[
         <div className="w-full flex justify-end">
-          <div style={{ width: "120px" }}>
-            <ButtonComponent onClick={handleClose}>Back</ButtonComponent>
-          </div>
+          <Button type="menu" onClick={handleClose}>Back</Button>
         </div>,
       ]}
     >
-      <div className="flex flex-col gap-y-4">
+      <div className="flex flex-col gap-y-4 p-4">
         <NxTable
           idTable="table-choose-contact"
           dataSource={dataTable}
@@ -251,12 +217,12 @@ const ModalChooseContact = ({
           useSelect={true}
           showAdvanceSearch={false}
           showSearchBar={true}
-          expandable={{ expandedRowRender }}
+          expandable={{ expandedRowRender: dataTable.length ? (record) => <ContactDetailExpandTable contactDetails={record.contactDetails} /> : undefined }}
           onSort={onSort}
           tableScrolled={{ y: 400, x: "max-content" }}
         />
       </div>
-    </ModalCustom>
+    </NxModal>
   );
 };
 
