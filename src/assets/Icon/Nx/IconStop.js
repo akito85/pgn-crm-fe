7b6edd2213b0b64154cab3316d06cebe15b56138
Vitrio Wibisono <vitrio@noxus.co.id>
2026-05-21 15:52:01 +0700

    feat(dashboard): hide tasklist for superuser/admin and show empty state
    - Restrict TasklistWidget to employee users only (userType === Employee && userLevel !== Super User)
    - Show Ant Design Empty component for non-employee/superuser dashboard
    - Normalize Nx icon components: use currentColor, consistent 24x24 viewBox, propagate onClick and color props

--- src/assets/Icon/Nx/IconStop.js
+++ src/assets/Icon/Nx/IconStop.js
@@ -4,10 +4,11 @@ const IconStop = ({
   height = "24",
   className = "",
   onClick = () => {},
+  color,
   ...otherProps
 }) => (
-  <svg className={`cursor-pointer ${className}`} style={style} width={width} height={height} {...otherProps} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M19 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+  <svg className={`cursor-pointer ${className}`} style={style} width={width} height={height} onClick={onClick} {...otherProps} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M19 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3Z" stroke={color || "currentColor"} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
   </svg>
 );
 
