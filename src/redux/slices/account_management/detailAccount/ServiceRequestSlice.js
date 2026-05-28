a6f808d2fad322738de44dc72a9e748170c6ab42
Jonathan Paskalis <ymjonathangp2@gmail.com>
2026-05-28 16:48:05 +0700

    fix(service request): draft detail - fixed get detail draft endpoint

--- src/redux/slices/account_management/detailAccount/ServiceRequestSlice.js
+++ src/redux/slices/account_management/detailAccount/ServiceRequestSlice.js
@@ -166,7 +166,7 @@ export const getServiceRequestDraft = createAsyncThunk(
   "GET_SERVICE_REQUEST_DRAFT",
   async ({ accountId, id }, thunkAPI) => {
     try {
-      const url = `/v1/dbs/api/accounts/${accountId}/servicerequests/${id}/draft`;
+      const url = `/v1/dbs/api/account/${accountId}/service-request/${id}/draft`;
       const response = await accountManagementService.getDetail(url);
       return response.data;
     } catch (error) {
